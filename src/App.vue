cf8e90cfbaa3de01aed43a946891b78ef5a3beeb
何其沆 <476358943@qq.com>
2024-06-04 11:26:26 +0800

    chore:封装a-table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
 <script setup>
+  import { ref } from 'vue'
+  import axios from 'axios'
   import HButton from './components/HButton.vue'
   import HDivider from './components/HDivider.vue'
+  import HTable from './components/HTable.vue'
+
+  const data = ref([])
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+    },
+    {
+      title: 'Gender',
+      dataIndex: 'genderName',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+    },
+  ];
+  const current = ref(1)
+  const pageSize = ref(1)
+  const total = ref(0)
+
+  function getData() {
+    axios.get(`http://localhost:3005/users/select?pageNum=${current.value}&pageSize=${pageSize.value}&name=`).then(res => {
+      return res.data
+    }).then(res => {
+      data.value = res.data
+      total.value = res.total
+    })
+  }
+
+  getData()
 </script>
 
 <template>
@@ -8,6 +45,24 @@
     <HButton size="small" danger ghost>按钮1</HButton>
     <HDivider dashed style="border-color: green;" type="vertical" />
     <HButton size="large" type="primary">按钮2</HButton>
+    <HTable bordered :columns="columns" :data-source="data" :pagination="{
+      total,
+      current,
+      pageSize,
+      onChange: (page) => {
+        current = page
+        getData()
+      }
+    }" style="margin-top: 12px;">
+      <template #name="{ text }">
+        <a>{{ text }}</a>
+      </template>
+      <template #age="{ text }">
+        <span>{{ text + 100 }}</span>
+      </template>
+      <template #title>Header111</template>
+      <template #footer>Footer111</template>
+    </HTable>
   </div>
 </template>
 
---
+++ src/components/HTable.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useSlots } from 'vue'
+
+const slots = Object.keys(useSlots()).filter(key => key !== 'title' && key !== 'footer')
+</script>
+
+<template>
+  <a-table>
+    <template #bodyCell="{ column, text, record, index }">
+      <template v-for="slot in slots" :key="slot">
+        <slot v-if="column.dataIndex === slot" :name="slot" v-bind="{ column, text, record, index }"></slot>
+      </template>
+    </template>
+    <template #title>
+      <slot name="title"></slot>
+    </template>
+    <template #footer>
+      <slot name="footer"></slot>
+    </template>
+  </a-table>
+</template>
+
+<style scoped>
+
+</style>
